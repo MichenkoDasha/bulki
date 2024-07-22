ddb5cb2b4ed063211b803e5a9627166ebeee1bf6
Sergei Michenko <michenko@bucklequick.com>
2024-07-22 20:27:11 +0300

    add dynamic products

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -2,66 +2,58 @@
     <q-page-container>
       <div class="q-pa-md">
         <div class="row justify-center q-gutter-md">
-
-            <q-card flat bordered class="my-card q-ma-lg">
-              <img src="~assets/bread1.jpg" alt="" />
-
+            <q-card
+              flat
+              bordered
+              class="my-card q-ma-lg"
+              v-for="p in products"
+              :key="p.title"
+            >
+              <img :src="'products/' + p.img" alt="" />
               <q-card-section>
-                <div class="text-h6">Хлеб пшеничный</div>
+                <div class="text-h6">{{ p.title }}</div>
                 <div class="text-subtitle2">
-                  Мука пшеничная в/с, мука пшеничная ц/з, вода, соль, мука рисовая.</div>
+                  {{ p.decr }}
+                </div>
               </q-card-section>
             </q-card>
-          <q-card flat bordered class="my-card q-ma-lg">
-            <img src="~assets/bread2.jpg" alt="" />
-
-            <q-card-section>
-              <div class="text-h6">Багет хрустящий</div>
-              <div class="text-subtitle2">Мука пшеничная в/с, вода питьевая, соль, дрожжи хлебопекарные прессованные, мука рисовая.</div>
-            </q-card-section>
-          </q-card>
-          <q-card flat bordered class="my-card q-ma-lg">
-            <img src="~assets/croissant.jpg" alt="" />
-
-            <q-card-section>
-              <div class="text-h6">Круассан классический</div>
-              <div class="text-subtitle2">Мука пшеничная в/с, глютен, инвертный сахар, соль, сахар, дрожжи, молоко 3.2%, масло сливочное.</div>
-            </q-card-section>
-          </q-card>
-          <q-card flat bordered class="my-card q-ma-lg">
-            <img src="~assets/cookie.jpg" alt="" />
-
-            <q-card-section>
-              <div class="text-h6">Card </div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-          </q-card>
-          <q-card flat bordered class="my-card q-ma-lg">
-            <img src="~assets/bread2.jpg" alt="" />
-
-            <q-card-section>
-              <div class="text-h6">Card </div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-          </q-card>
-          <q-card flat bordered class="my-card q-ma-lg">
-            <img src="~assets/bread2.jpg" alt="" />
-
-            <q-card-section>
-              <div class="text-h6">Card </div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-          </q-card>
         </div>
       </div>
     </q-page-container>
 </template>
 
 <script setup>
+import {ref} from "vue";
+
 defineOptions({
   name: 'ProductsPage'
 });
+
+const products = ref([
+  {
+    img: 'bread1.jpg',
+    title: 'Хлеб пшеничный',
+    decr: 'Мука пшеничная в/с, мука пшеничная ц/з, вода, соль, мука рисовая.'
+  },
+  {
+    img: 'bread2.jpg',
+    title: 'Багет хрустящий',
+    decr: 'Мука пшеничная в/с, вода питьевая, соль, дрожжи хлебопекарные прессованные, мука рисовая.'
+  },
+  {
+    img: 'croissant.jpg',
+    title: 'Круассан классический',
+    decr: 'Мука пшеничная в/с, глютен, инвертный сахар, соль, сахар, дрожжи, молоко 3.2%, масло сливочное.'
+  },
+  {
+    img: 'cookie.jpg',
+    title: 'Хлеб пшеничный',
+    decr: 'Мука пшеничная в/с, мука пшеничная ц/з, вода, соль, мука рисовая.'
+  }
+])
+
 </script>
+
 <style lang="sass" scoped>
 .my-card
   width: 100%
